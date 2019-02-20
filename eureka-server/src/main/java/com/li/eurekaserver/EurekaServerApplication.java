e579805787c21c63d9721ad5ceab897291d1f72a
lihaodong <lihaodongmail@163.com>
2019-02-20 22:56:46 +0800

    服务治理,服务注册与发现
    服务注册与发现

---
+++ eureka-server/src/main/java/com/li/eurekaserver/EurekaServerApplication.java
@@ -0,0 +1,17 @@
+package com.li.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+//通过@EnableEurekaServer注解启动服务注册中心
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+
+}
+
